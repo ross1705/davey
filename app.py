f9f316d205a33a6eb9cb4d4c67416189abecdd97
ross1705 <rossjackson1705@gmail.com>
2023-12-06 20:01:04 +0000

    db updates

--- app.py
+++ app.py
@@ -3,20 +3,22 @@ import openai
 import time
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import json
-import os
+import sqlite3
 
 def save_conversation(user_id, user_input, bot_response):
-    filename = f"data/{user_id}.json"
-    conversation = []
+    try:
+        with sqlite3.connect('my_telegram_bot.db') as conn:
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT INTO conversations (user_id, user_input, bot_response)
+                VALUES (?, ?, ?)
+            ''', (user_id, user_input, bot_response))
+            conn.commit()
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+    except Exception as e:
+        print(f"Exception in _query: {e}")
 
-    if os.path.exists(filename):
-        with open(filename, 'r') as file:
-            conversation = json.load(file)
-    
-    conversation.append({'user_input': user_input, 'bot_response': bot_response})
-    
-    with open(filename, 'w') as file:
-        json.dump(conversation, file)
 
 # Set API keys
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -76,7 +78,7 @@ def handle_message(update, context):
         update.message.reply_text(bot_response)
         # Save the conversation after sending the response
         save_conversation(user_id, user_input, bot_response)
-        
+
 # New function to add a message to an existing thread
 def add_to_thread(thread_id, prompt):
     openai.beta.threads.messages.create(
@@ -95,6 +97,18 @@ def start(update, context):
     update.message.reply_text('Hello! How can I assist you today?')
 
 def main():
+    with sqlite3.connect('my_telegram_bot.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS conversations (
+                user_id INTEGER,
+                user_input TEXT,
+                bot_response TEXT,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+            )
+        ''')
+        conn.commit()
+    
     updater = Updater(telegram_token, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
